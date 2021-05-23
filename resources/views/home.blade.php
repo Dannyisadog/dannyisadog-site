de0f8b3399b39f48bbe4344e78866afc35f912a8
Dannyisadog <dannyisadog10@gmail.com>
2021-05-23 18:28:34 +0800

    feat: todo list root dom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+            <div id="root"></div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('js-down')
+<script src='/js/home.js'></script>
 @endsection
