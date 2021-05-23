a9cbf41412b615ed63cd0817c0a51c2c79da43b2
Dannyisadog <dannyisadog10@gmail.com>
2021-05-23 23:01:24 +0800

    feat: back-end route for create and fetch todo list

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/fetchTodoList', [App\Http\Controllers\TodoListController::class, 'fetchTodoList']);
+Route::post('/createTodoList', [App\Http\Controllers\TodoListController::class, 'createTodoList']);
 
 Auth::routes();
 
