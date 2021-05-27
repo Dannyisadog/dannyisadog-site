f330554430a7b89da8ba914ee901313172505fbc
Dannyisadog <dannyisadog10@gmail.com>
2021-05-28 01:48:50 +0800

    feat: todolist migration

---
+++ database/migrations/2021_05_27_174251_create_todo_list_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_list', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string("title", 255);
+            $table->timestamp("deleted_at");
+            $table->timestamp("created_at");
+            $table->timestamp("updated_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_list');
+    }
+}
