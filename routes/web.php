72474ad6c9a8691bcd8421dd934a58b776670546
Dannyisadog <dannyisadog10@gmail.com>
2021-05-30 14:15:44 +0800

    fix: home route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\Controller::class, 'index']);
+Route::get('/home', [App\Http\Controllers\Controller::class, 'index']);
 Route::get('/todolist', [App\Http\Controllers\HomeController::class, 'index']);
 
 // todo list
