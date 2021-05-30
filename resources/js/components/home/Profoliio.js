69f7cb9c9e71bb71de549feba5311b3b300305c6
Dannyisadog <dannyisadog10@gmail.com>
2021-05-30 23:18:34 +0800

    feat: profolio component

--- resources/js/components/home/Profoliio.js
+++ resources/js/components/home/Profoliio.js
@@ -2,13 +2,45 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     width: 100%;
-    height: 100px;
-    border:1px solid #333;
+    border:0;
     border-radius: 8px;
     padding: 20px;
+    background: linear-gradient(to right, #344f81, #A8D0E6);
     
     .title {
-        font-size: 24px;
+        font-size: 32px;
+        color: white;
+        font-weight: bold;
+    }
+
+    .line { 
+        width: 100%;
+        height: 3px;
+        background: linear-gradient(to right, white, #A8D0E6);;
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+
+    .content {
+        color: white;
+        .about {
+            display: flex;
+        }
+        .basic-info {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-left: 20px;
+            font-size: 16px;
+            .email {
+                color: white;
+                text-decoration: none !important;
+                transition: .3s;
+            }
+            .email:hover {
+                color: #f76c6c;
+            }
+        }
     }
 `;
 
@@ -18,8 +50,28 @@ const Profolio = () => {
             <div className="title">
                 profolio
             </div>
+            <div className="line"></div>
             <div className="content">
-
+                <div className="about">
+                    <div className="profile-img"></div>
+                    <div className="basic-info">
+                        <div className="name">
+                            陳鍾逸 Danny
+                        </div>
+                        <div className="age">
+                            25歲
+                        </div>
+                        <div className="gender">
+                            男生
+                        </div>
+                        <div className="job-title">
+                            網站工程師
+                        </div>
+                        <div className="email">
+                            <a target="_blank" className="email" href = "mailto: dannyisadog10@gmail.com">dannyisadog10@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
     );
