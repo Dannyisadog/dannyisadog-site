70cf37b2f8eb0c6be05dfb7d9ebcfbc40a5e8a69
Danny <dannyisadog10@gmail.com>
2022-05-27 19:04:49 +0800

    feat: favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,">
 
-    <link rel="icon" href="{{ asset('images/logo-light.svg') }}">
+    <link rel="icon" href="{{ asset('images/logo.svg') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
